c71ddd4e915993c66ce55b8c72db62bc6f738811
JoseGabrielZ <66148656+JoseGabrielZ@users.noreply.github.com>
2020-05-30 10:09:56 -0300

    Update so.asm

--- so.asm
+++ so.asm
@@ -11,6 +11,20 @@ MOV BH, 0x00 ; PAGINA
 MOV CX, 0x00 ; X
 MOV DX, 0x00 ; Y
 
+; =======================================================================================================================
+; Basicamente a subfunção de AH 0x0C troca a cor pro pixel na posição X armazenada em CX
+; e na posição Y amazenada em DX, para a cor de AL
+; toda vez que o loop for acionado, vai ocorrer duas coisas, incremento de 1 ou mais registradores e
+; comparações dos valores desses registradores
+; primeiramente sera checado o valor de CX, se ele tiver chegado ao tamanho maximo da linha 0x140
+; ele vai incrementar em 1 o valor de DX, ou seja, da "linha"
+; após ele pular uma linha o valor de CX vai voltar a 0x00 e começar a preencher a nova linha pelo primeiro pixel
+; e assim por diante até completar a tela
+; após isso o valor de AL sera icrementado e a cor trocada
+; os registradores CX e DX volta a ser 0 para começar a preencher tudo de novo
+; se o valor de AL chegar ao limite 0xFF, é retornado a 0x00
+; =======================================================================================================================
+
 LOOP:
 	MOV AH, 0x0C
 	INT 0x10
